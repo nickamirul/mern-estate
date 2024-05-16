adfa0494fa7a1d9fb6e39b0c0056910aa312db73
nickamirul <nickamirul9945@gmail.com>
2024-05-16 09:06:06 +0800

    create sign in api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -41,7 +41,7 @@ const SignUp = () => {
     }
   };
 
-  console.log(formData);
+  // console.log(formData);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
